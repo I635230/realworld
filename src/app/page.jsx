11b5f06586ec1b9d714f4177d47dc7ba505374a0
I635230 <yugo.susuki@gmail.com>
2024-01-16 10:29:58 +0900

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/app/ui/header";
+import Footer from "@/app/ui/footer";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
