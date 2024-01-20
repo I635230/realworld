d7aa2c6e56b94d60feaf1245947d1de496293833
I635230 <yugo.susuki@gmail.com>
2024-01-20 10:52:29 +0900

    :sparkles: ログイン機能を追加 fixes #21

---
+++ src/app/lib/auth.js
@@ -0,0 +1,31 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function authenticate(state, formData) {
+  try {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const response = await fetch(`http://api:3000/api/users/login`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        user: {
+          email: email,
+          password: password,
+        },
+      }),
+    });
+    const data = await response.json();
+    const token = data.user.token;
+    cookies().set("session", token, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7, // One week
+      path: "/",
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    console.log("ログインエラー");
+  }
+}
